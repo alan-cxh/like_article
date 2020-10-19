54a579bbdb0c0b2c1c90590eccebbdc3b85f291d
alan-cxh <cxh_928765@163.com>
2020-10-19 22:58:21 +0800

    Signed-off-by: alan-cxh <cxh_928765@163.com>

---
+++ redis-like-web/src/main/java/com/h2t/study/Runner.java
@@ -0,0 +1,17 @@
+package com.h2t.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * 项目启动类
+ */
+@SpringBootApplication
+@EnableScheduling
+public class Runner {
+    public static void main(String[] args) {
+        SpringApplication.run(Runner.class, args);
+    }
+}
